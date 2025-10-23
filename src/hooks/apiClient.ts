7e8e90624006372ce952e3ee296ff9a4b96f00e1
Pham Phong <phamphong9981@gmail.com>
2025-10-23 08:29:14 +0000

    fix

--- src/hooks/apiClient.ts
+++ src/hooks/apiClient.ts
@@ -1,3 +1,5 @@
+'use client'
+
 import axios, { AxiosResponse } from 'axios'
 
 // Create axios instance with default config
--- src/hooks/useWeek.ts
+++
@@ -1,6 +0,0 @@
-export interface Week {
-    id: string
-    startDate: string
-    endDate: string
-    status: string
-}
